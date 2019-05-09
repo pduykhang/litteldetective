521dff02401a9394387269268da511e0e92d494c
pdkhang <khangy9a1@gmail.com>
2019-05-09 10:47:28 +0700

    connect to elastic search and mongodb server

---
+++ internal/dataconnection/mongoDB.go
@@ -0,0 +1,20 @@
+package dataconnection
+
+import (
+	"github.com/PhamDuyKhang/littledetective/internal/pkg/flog"
+	"github.com/globalsign/mgo"
+)
+
+func GetConnection() (*mgo.Session, error) {
+	l := flog.New()
+	l.SetLocal("dataconnection")
+
+	mgoss, err := mgo.Dial("mongodb:27017")
+	if err != nil {
+		l.Errorf("have error when connect to database with")
+		return nil, err
+	}
+	mgoss.SetMode(mgo.Monotonic, true)
+	l.Errorf("connection to mongodb at %v is successfully", "mongodb:27017")
+	return mgoss, nil
+}
