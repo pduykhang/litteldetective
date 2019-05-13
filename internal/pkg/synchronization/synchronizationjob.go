19e9653076038a2c61275e17f2dd78a1f444b8a6
pdkhang <khangy9a1@gmail.com>
2019-05-13 17:26:07 +0700

    the job is implemented later

--- internal/pkg/synchronization/synchronizationjob.go
+++ internal/pkg/synchronization/synchronizationjob.go
@@ -1,28 +1,25 @@
 package synchronization
 
-import (
-	"github.com/PhamDuyKhang/littledetective/internal/film"
-	"github.com/PhamDuyKhang/littledetective/internal/pkg/flog"
-)
-
-func Sync(filmID chan string, elService *film.FilmService, mongoService *film.FilmMongoService) {
-	logger := flog.New()
-	logger.SetLocal("synchronization")
-	for {
-		select {
-		case idFilm, ok := <-filmID:
-			if !ok {
-				return
-			}
-			tem, err := mongoService.GetFilmByID(idFilm)
-			if err != nil {
-			} else {
-				_, err := elService.InsertDataToElastic(tem)
-				if err != nil {
-
-				}
-			}
-
-		}
-	}
-}
+// This func still not completion
+//func Sync(filmID chan string, elService *film.FilmService, mongoService *film.FilmMongoService) {
+//	logger := flog.New()
+//	logger.SetLocal("synchronization")
+//	for {
+//		select {
+//		case idFilm, ok := <-filmID:
+//			if !ok {
+//				return
+//			}
+//			tem, err := mongoService.GetFilmByID(idFilm)
+//			if err != nil {
+//
+//			} else {
+//				_, err := elService.InsertDataToElastic(tem)
+//				if err != nil {
+//
+//				}
+//			}
+//
+//		}
+//	}
+//}
