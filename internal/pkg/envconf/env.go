a42de17bca93706faa440f481ed78c6a1f189450
pdkhang <khangy9a1@gmail.com>
2019-05-13 17:20:25 +0700

    env loader

--- internal/pkg/envconf/env.go
+++ internal/pkg/envconf/env.go
@@ -5,13 +5,13 @@ import (
 	"github.com/kelseyhightower/envconfig"
 )
 
-func Load(envstruct interface{}) {
+func Load(envstruct interface{}) error {
 	l := flog.New()
 	l.SetLocal("envconf")
 	if err := envconfig.Process("", envstruct); err != nil {
 		l.Errorf("can't get config from environment variable,with err = %v ", err)
-		return
+		return err
 	}
 	l.Logger.Infof("get env variable successfully")
-	return
+	return nil
 }
