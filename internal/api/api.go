206cc685a1effbc75cd0636f0445d8d275fa614b
pdkhang <khangy9a1@gmail.com>
2019-05-13 17:09:25 +0700

    restructure api, make it manage easily

--- internal/api/api.go
+++ internal/api/api.go
@@ -5,46 +5,100 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"github.com/PhamDuyKhang/littledetective/internal/dataconnection"
+	"github.com/PhamDuyKhang/littledetective/internal/externalconnection"
 	"github.com/PhamDuyKhang/littledetective/internal/film"
 	"github.com/PhamDuyKhang/littledetective/internal/handler"
 	"github.com/PhamDuyKhang/littledetective/internal/pkg/flog"
 )
 
-func Init() *mux.Router {
+const (
+	get  = http.MethodGet
+	post = http.MethodPost
+)
 
+type (
+	ExternalConn struct {
+		Database     externalconnection.DBConnection
+		SearchEngine externalconnection.SearchConnection
+	}
+	middlewareFunc = func(http.HandlerFunc) http.HandlerFunc
+	route          struct {
+		url        string
+		method     string
+		handler    http.HandlerFunc
+		middleware []middlewareFunc
+	}
+)
+
+func Init(exc *ExternalConn) *mux.Router {
 	logger := flog.New()
 	logger.SetLocal("api")
 	// get connection, that is all database is supported
 	// Elastic search
-	client, err := dataconnection.GetElasticConnection()
-	if err != nil {
-		logger.Errorf("can't connect to elastic service with err:= %v", err)
-	}
-	elasticRepo := film.NewElasticFilmRepository(client)
-	SearchService := film.NewFilmService(elasticRepo, logger)
 
-	searchHandler := handler.NewSearchHandeler(SearchService, logger)
+	elasticRepo := film.NewElasticFilmRepository(exc.SearchEngine.Client)
+	searchService := film.NewFilmService(elasticRepo, logger)
+
+	searchHandler := handler.NewSearchHandler(searchService, logger)
 
 	//mongodb
-	mgoSession, err := dataconnection.GetConnection()
-	if err != nil {
-		logger.Errorf("can't connect to mongodb service with err:= %v", err)
-	}
-	mongoRepo := film.NewFilmMongoRepository(mgoSession)
+	mongoRepo := film.NewFilmMongoRepository(exc.Database.Session)
 	mongoService := film.NewFilmMongoService(mongoRepo)
-	filmHandler := handler.NewFilmHandler(mongoService, logger, SearchService)
+	filmHandler := handler.NewFilmHandler(mongoService, logger, searchService)
 
 	r := mux.NewRouter()
-	r.HandleFunc("/search", searchHandler.Search)
-	r.HandleFunc("/search/add", searchHandler.AddFilmToElastic).Methods(http.MethodPost)
-	r.HandleFunc("/test", searchHandler.Test)
-	//
-	r.HandleFunc("/film", filmHandler.GetFilm).Methods(http.MethodGet)
-	r.HandleFunc("/make", filmHandler.GenerateData).Methods(http.MethodGet)
-	r.HandleFunc("/film/{id}", filmHandler.GetFilmWithID).Methods(http.MethodGet)
-	r.HandleFunc("/film", filmHandler.AddFilm).Methods(http.MethodPost)
-	r.HandleFunc("/sync", filmHandler.Sync).Methods(http.MethodGet)
+
+	routes := []route{
+		{
+			url:     "/api/v1/test",
+			handler: searchHandler.Test,
+			method:  get,
+		},
+		{
+			url:     "/api/v1/films/init",
+			handler: filmHandler.GenerateData,
+			method:  get,
+		},
+		{
+			url:     "/api/v1/films/sync",
+			handler: filmHandler.Sync,
+			method:  get,
+		},
+		{
+			url:     "/api/v1/films/search",
+			handler: searchHandler.Search,
+			method:  get,
+		},
+		{
+			url:     "/api/v1/films",
+			handler: filmHandler.GetFilm,
+			method:  get,
+		},
+		{
+			url:     "/api/v1/films",
+			handler: filmHandler.AddFilm,
+			method:  post,
+		},
+		{
+			url:     "/api/v1/film/{id}",
+			handler: filmHandler.GetFilmWithID,
+			method:  get,
+		},
+		{
+			url:     "/api/v1/film/search",
+			handler: searchHandler.AddFilmToElastic,
+			method:  post,
+		},
+	}
+	logger.Infof("init router ")
+
+	for _, rt := range routes {
+		h := rt.handler
+		for i := len(rt.middleware) - 1; i >= 0; i-- {
+			h = rt.middleware[i](h)
+		}
+		r.Path(rt.url).Methods(rt.method).HandlerFunc(h)
+	}
 	return r
 
 }
